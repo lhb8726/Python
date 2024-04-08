948330c2eb245012c7cd5cd9469250e3bd7686d0
lhb8726 <lhb5329@naver.com>
2024-04-08 13:42:01 +0900

    Do it! Jump to Python

---
+++ Do it! Jump to Python/input.py
@@ -0,0 +1,29 @@
+# Input
+a = input()
+print(a)
+
+# 프롬프트를 띄어 사용자 입력받기
+b = input("숫자를 입력하세요 : ")
+print(b)
+
+# Print
+c = 123
+print(c)
+
+c = "Python"
+print(c)
+
+c = [1, 2, 3]
+print(c)
+
+# 큰따옴표로 둘러싸인 문자열은 + 연산과 동일
+print("Life" "is" "too short")
+
+print("Life"+"is"+"too short")
+
+# 문자열 띄어쓰기는 쉼표
+print("Life", "is", "too short")
+
+# 한 줄에 결괏값 출력
+for i in range(10):
+    print(i, end= " ")
